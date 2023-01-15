97eb4c93818a64a00494b73377127a5090209d43
Pragati-Bhadouriya <114304854+Pragati-Bhadouriya@users.noreply.github.com>
2023-01-15 19:43:45 +0530

    Add files via upload

---
+++ Lab5/src/EmployeeManagement/repositry/EmployeeRepositry.java
@@ -0,0 +1,9 @@
+package EmployeeManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatlearning.employeemanagement.model.*;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+}
